356dc917b7fd61bf98dc9015d4f4146624cd9108
Denis Ojok <Denis.Ojok@gou.go.ug>
2020-07-26 07:51:20 +0300

    Added Comments to zengaPayAPI

--- zengaPayAPI.php
+++ zengaPayAPI.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class zengaPayAPI
 {
     private $_host;
@@ -10,13 +9,33 @@ class zengaPayAPI
     private $_resource;
     private $_request;
 
+    /**
+     * zengaPayPI constructor.
+     * @param string $host
+     * @param string $APIVersion
+     * @param string $protocol
+     */
+    
     public function __construct($host, $APIVersion ="/v1",$protocol = 'https')
     {
         $this->_host = $host;
         $this->_APIVersion = $APIVersion;
         $this->_protocol = $protocol;
     }
-
+    /**
+     * Request Funds from a Mobile Money User, The requested funds will be deposited into your account
+     * Shortly after you submit this request, the mobile money user receives an on-screen
+     * notification on their mobile phone. The notification informs the mobile money user about
+     * your request to transfer funds out of their account and requests them to authorize the
+     * request to complete the transaction.
+     * This request is not supported by all mobile money operator networks
+     * @param string $msisdn: The mobile money phone number in the format 256772123456
+     * @param double $amount: The amount of money to be deposited into your account (floats are supported)
+     * @param string $external_reference: Something which yourself and the beneficiary agree upon e.g. an invoice number
+     * @param string $narration: The reason for the mobile money user to deposit funds
+     * @return array
+     */
+    
     public function requestPayment($msisdn,$amount,$external_reference,$narration)
     {
         $this->_resource = "/collections";
@@ -28,14 +47,40 @@ class zengaPayAPI
         );
         return $this->sendAPIRequest('POST',$this->_resource,json_encode($this->_request));
     }
+    /**
+     * Check the status of a single transaction that was earlier submitted for processing.
+     * It can also be used to check on any other transaction on the system.
+     * @param string $transactionReference: The reference to the transaction whose status you would like to follow up on. This is typically the transaction reference which came through as part of an earlier collection request response.
+     * @return object
+     */
+    
     public function getSingleCollection($transactionReference)
     {
         return $this->sendAPIRequest('GET',"/collections/{$transactionReference}");
     }
+    /**
+     * Fetch all collections that were earlier submitted for processing.
+     * @return object
+     */
+    
     public function getAllCollections()
     {
         return $this->sendAPIRequest('GET',"/collections");
     }
+    /**
+     * Transfer funds from your ZENGAPAY Account to a mobile money user
+     * This transaction transfers funds from your ZENGAPAY Account to a mobile money user.
+     * Please handle this request with care because if compromised, it can lead to
+     * withdrawal of funds from your account.
+     * This request is not supported by all mobile money operator networks
+     * This request requires permission that is granted by the specific IP Address(es) whitelisted in your ZENGAPAY Dashboard
+     * @param string $msisdn the mobile money phone number in the format 256772123456
+     * @param double $amount: The amount of money to withdraw from your account (floats are supported)
+     * @param string $external_reference: Something which yourself and the beneficiary agree upon e.g. an invoice number
+     * @param string $narration: The reason for the mobile money user to deposit funds
+     * @return array
+     */
+    
     public function sendTransfer($msisdn,$amount,$external_reference,$narration)
     {
         $this->_resource = "/transfers";
@@ -47,31 +92,84 @@ class zengaPayAPI
         );
         return $this->sendAPIRequest('POST',$this->_resource,json_encode($this->_request));
     }
+    /**
+     * Check the status of a single transaction that was earlier submitted for processing.
+     * It can also be used to check on any other transaction on the system.
+     * @param string $transactionReference: The reference to the transaction whose status you would like to follow up on. This is typically the transaction reference which came through as part of an earlier transfer request response.
+     * @return object
+     */
+    
     public function getSingleTransfer($transactionReference)
     {
         return $this->sendAPIRequest('GET',"/transfers/{$transactionReference}");
     }
+    /**
+     * Fetch all transfers that were earlier submitted for processing.
+     * @return object
+     */
+    
     public function getAllTransfers()
     {
         return $this->sendAPIRequest('GET',"/transfers");
     }
+    /**
+     * Get the current balance of your ZENGAPAY Account
+     * Returns objects contains an array of balances (including airtime)
+     * @return object
+     */
+    
     public function accountGetBalance()
     {
         return $this->sendAPIRequest('GET',"/account/balance");
     }
-    public function accountGetStatement()
+    /**
+     * Return an account statement object of transactions which were carried out on your account for a certain period of time
+     * @param string $start format YYYY-MM-DD HH:MM:SS
+     * @param string $end  format YYYY-MM-DD HH:MM:SS
+     * @param string $status
+     * Options
+     * * "FAILED"
+     * * "PENDING"
+     * * "INDETERMINATE"
+     * * "SUCCEEDED"
+     * * "FAILED,SUCCEEDED" (comma separated)
+     * @param string $currency_code
+     * Options
+     * * "UGX-MTNMM" -> Uganda Shillings - MTN Mobile Money
+     * * "UGX-ATLMM" -> Uganda Shillings - Airtel Money
+     * @param int $limit Default limit = 25
+     * @param string $designation
+     * Options
+     * * "TRANSACTION"
+     * * "CHARGES"
+     * * "ANY"
+     * @return object
+     */
+    
+    public function accountGetStatement($start=NULL, $end=NULL, $status=NULL, $currency_code=NULL, $limit=NULL, $designation='ANY')
     {
         return $this->sendAPIRequest('GET',"/account/statement");
     }
     /**
-     * Define secret key for alternative authentication
+     * Define API key for authentication
      *
      * @param string $APIKey
      */
+    
     public function setAPIKey($APIKey)
     {
         $this->_APIKey = $APIKey;
     }
+
+    /**
+     * Clean Amount before passing to API
+     * @param string $number
+     * @param bool $force_number
+     * @param string $dec_point
+     * @param string $thousands_sep
+     * @return int or float
+     */
+    
     private function unformat($number, $force_number = true, $dec_point = '.', $thousands_sep = ',') {
         if ($force_number) {
             $number = preg_replace('/^[^\d]+/', '', $number);
@@ -89,6 +187,7 @@ class zengaPayAPI
      *
      * @return array
      */
+
     private function _setHeaders()
     {
         $headers = array(
@@ -109,6 +208,7 @@ class zengaPayAPI
      * @param string $body
      * @return object
      */
+    
     protected function sendAPIRequest($method,$endPoint,$body=NULL)
     {
         $ch = curl_init( );
